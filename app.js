c7bae62621cced15fbcfe8ca3ea2815eb54927a6
PhoebeMay <phoebenichols@gmail.com>
2015-08-14 20:12:37 +0100

    googog

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.post('/score', function(request, response){
 
 //(process.env.PORT || 8080)
 
-var server = app.listen((8080), function() {
+var server = app.listen((process.env.PORT || 8080), function() {
     var host = server.address().address;
     var port = server.address().port;
 
